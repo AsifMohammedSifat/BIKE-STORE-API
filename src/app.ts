940feca95c933775164389ba4c72a02fef647a2c
AsifMohammedSifat <asifmohammedsifat38@gmail.com>
2024-12-17 03:05:22 +0600

    Add message to home page

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+  res.send('Welcome to Bike Store API');
 });
 
 // Prodcut route
